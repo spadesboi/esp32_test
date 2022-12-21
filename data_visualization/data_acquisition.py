688899bd1966a7967268af0ae5fe90e7f5478984
natitomato <samarthsudhi67@gmail.com>
2022-12-21 15:07:12 +0530

    Fixed data_acc code, added raw data json file

--- data_visualization/data_acquisition.py
+++ data_visualization/data_acquisition.py
@@ -7,7 +7,7 @@ import sys
 from matplotlib import pyplot as plt
 import numpy as np
 
-data_acquisition = 1 # 1 --> acquisition on, 0 --> no acquisition, only visualization of saved data
+data_acquisition = 0 # 1 --> acquisition on, 0 --> no acquisition, only visualization of saved data
 n = 1000 # no of lines to be read
 
 write_data = []
@@ -62,50 +62,27 @@ if data_acquisition == 1:
 
 #------------------visualizing data---------------------
 
+count_arr = []
+time_to_ping = []
 if data_acquisition == 0:
     print("Analysing data")
     with open(data_save_directory+file_name+".json","r") as readfile:
         read_data = json.load(readfile)
         read_data = np.array(read_data, object).T.tolist() # convert read data to nparray, use T attribute to transpose and convert back to list
-    #     
     #     print(read_data)
         for i in range(3,1000):
             final_split = read_data[i].split('_')
-            final
+            count_arr.append(final_split[0])
+            time_to_ping.append(final_split[2])
 
-
-            if (avg_num!=1):
-                adc_avg_list.append(read_data[i:i+avg_num])
-            else:
-                adc_avg_list.append(read_data[i])
-#             adcValue.insert(i, read_data[i])            
-    #         print(adcValue)
-#             adcVoltage.insert(i, (float(adcValue[i]) / 1024.0) * 5)
-#             currentValue.insert(i, (offsetVoltage - adcVoltage[i]) / sensitivity)
-#             currentVal.insert(i, [adcValue, adcVoltage, currentValue])
-    #         count+=1
-    #         print(count)
-            
-        for i in adc_avg_list:
-            sum = 0
-            if (avg_num!=1):
-                for j in i:
-                    sum = sum + j  
-                sum = sum/avg_num
-            else:
-                # for j in i:
-                    sum = i
-            adc_avg_sum_list.append(sum)
         print("Data analysing succesful.")
-        with open(data_save_directory+file_name_2+"_with_avgsize_"+str(avg_num)+".json", "w") as outfile:
-            json.dump(adc_avg_sum_list, outfile)
-                
-    print("Analysed data written to file: ", file_name_2, "_with_avgsize_"+str(avg_num))
     #sys.exit()
 
     # Adapt the plot as required everytime before running
     # to plot R1/R2/R3 values and states
-    plt.plot(adc_avg_sum_list, color='r', label='ADC Value')
+    # plt.plot(count_arr, color='r', label='Packet')
+    plt.plot(time_to_ping, color='b', label='ping_time')
+    plt.ylim([0,50])
 #     plt.plot(currentValue, color='b', label='R2 value')
     #plt.plot(read_data[5][:], color='y', label='R3 value')
     #plt.plot([x for x in range(0,n)], [x*100 for x in read_data[0][:]], color='r', label='R1 state')
@@ -119,14 +96,6 @@ if data_acquisition == 0:
 
     #plt.title(image_title)
     #plt.legend()
-    plt.savefig(data_save_directory+file_name_2+"_with_avgsize_"+str(avg_num)+".png")
+    plt.savefig(data_save_directory+file_name_2+".png")
     plt.grid(True)
     plt.show()
-
-    #try:
-     #   while True:
-      #      cmd = input("Enter command :")
-       #     arduino_serial.write(cmd.encode())
-    #except KeyboardInterrupt:
-     #   print("KeyboardInterrupt has been caught. Program terminated.") 
-
